35fcd5065314c5dd328bb31826d164ddf8ac8f69
10kTonn <71646248+10kTonn@users.noreply.github.com>
2024-07-30 13:26:04 +0300

    Initial Commit

---
+++ WorkingProjecr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingProjecr
+//
+//  Created by User on 30.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // some code
+    }
+
+
+}
+
